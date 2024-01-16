53b48cdfbac2a0dcb3707fab88ed64d79027900e
Ula <ursz.kam@gmail.com>
2024-01-16 13:38:16 +0100

    add function  strncmp

---
+++ ft_strncmp.c
@@ -0,0 +1,16 @@
+/*
+** function compares at most the first n bytes of the two strings s1 and s2.
+** It returns an integer less than, equal to, or greater than zero 
+** if s1 is found, respectively, to be less than, to match,
+** or be greater than s2
+*/
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+    int	i;
+
+    i = 0;
+    while (s1[i] == s2[i] && s1[i] && i < n - 1)
+        i++;
+    return (s1[i] - s2[i]);
+}
