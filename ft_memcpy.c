15677f91fa7e3c730834270dc7f0a15aedba3c17
Ula <ursz.kam@gmail.com>
2024-01-16 19:22:53 +0100

    add func memcpy

---
+++ ft_memcpy.c
@@ -0,0 +1,17 @@
+
+
+#include "libft.h"
+
+void *memcpy(void *dest, const void *src, size_t n)
+{
+    unsigned char *dest_ptr;
+    unsigned char *src_ptr;
+
+    if (!dest && !src)
+        return (0);
+    dest_ptr = (unsigned char *)dest;
+    src_ptr = (unsigned char *)src;
+    while (n-- > 0)
+        *(dest_ptr++) = *src_ptr++;
+    return (dest);
+}
