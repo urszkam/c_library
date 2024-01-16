34bc31a2100d3e73df07a315f1e709e7307d3c70
Ula <ursz.kam@gmail.com>
2024-01-16 19:22:34 +0100

    update prototypes

--- libft.h
+++ libft.h
@@ -1,5 +1,5 @@
 #ifndef libft_H
-# define libft_H
+#define libft_H
 
 #include <stdlib.h>
 
@@ -15,13 +15,14 @@ int ft_tolower(int c);
 int ft_strncmp(const char *s1, const char *s2, size_t n);
 int ft_atoi(const char *nptr);
 
-size_t  ft_strlen(const char *s);
-size_t  ft_strlcat(char *dst, const char *src, size_t size);
-size_t  ft_strlcpy(char *dst, const char *src, size_t size);
+size_t ft_strlen(const char *s);
+size_t ft_strlcat(char *dst, const char *src, size_t size);
+size_t ft_strlcpy(char *dst, const char *src, size_t size);
 
-char    *ft_strchr(const char *s, int c);
-char    *ft_strrchr(const char *s, int c);
+char *ft_strchr(const char *s, int c);
+char *ft_strrchr(const char *s, int c);
 
-void    ft_bzero(void *s, size_t n);
-void    *ft_memset(void *s, int c, size_t n);
+void ft_bzero(void *s, size_t n);
+void *ft_memset(void *s, int c, size_t n);
+void *ft_memcpy(void *dest, const void *src, size_t n);
 #endif
