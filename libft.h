03803e2c77ebb71468668dd6e2bc56c7b44edaf7
Ula <ursz.kam@gmail.com>
2024-01-16 16:28:39 +0100

    update prototypes

--- libft.h
+++ libft.h
@@ -13,11 +13,15 @@ int ft_isprint(int c);
 int ft_toupper(int c);
 int ft_tolower(int c);
 int ft_strncmp(const char *s1, const char *s2, size_t n);
+int ft_atoi(const char *nptr);
 
 size_t  ft_strlen(const char *s);
-size_t  ft_strlcat(char *dst, const char *src, size_t size)
+size_t  ft_strlcat(char *dst, const char *src, size_t size);
 size_t  ft_strlcpy(char *dst, const char *src, size_t size);
 
 char    *ft_strchr(const char *s, int c);
 char    *ft_strrchr(const char *s, int c);
+
+void    ft_bzero(void *s, size_t n);
+void    *ft_memset(void *s, int c, size_t n);
 #endif
