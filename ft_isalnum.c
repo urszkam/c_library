aebf33c1ab3f89a0f1baa86672c24b846f673a93
Ula <ursz.kam@gmail.com>
2024-01-16 13:36:57 +0100

    style: change space to tab

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -3,7 +3,7 @@
 ** it is equivalent to (isalpha(c) || isdigit(c))
 */
 
-int ft_isalnum(int c)
+int	ft_isalnum(int c)
 {
     if ((c >= '0' && c <= '9') || (c >= 'a' && c <= 'z') 
         || (c >= 'A' && c <= 'Z'))
