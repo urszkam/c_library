9ca6035672e329b47fd4d1d02ee306b3f4361ecb
Ula <ursz.kam@gmail.com>
2024-01-20 01:19:10 +0100

    add func putendl

---
+++ ft_putendl_fd.c
@@ -0,0 +1,11 @@
+/*
+**Outputs the string ’s’ to the given file descriptor followed by a newline.
+*/
+
+#include "libft.h"
+
+void ft_putendl_fd(char *s, int fd)
+{
+    ft_putstr_fd(s, fd);
+    ft_putchar_fd('\n');
+}
