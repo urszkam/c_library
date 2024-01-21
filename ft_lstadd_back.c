953ea607639ce4d89ff99642cde858c691625f6e
Ula <ursz.kam@gmail.com>
2024-01-21 21:05:37 +0100

    add func lstadd_back

---
+++ ft_lstadd_back.c
@@ -0,0 +1,11 @@
+#include "libft.h"
+
+void ft_lstadd_back(t_list **lst, t_list *new)
+{
+    t_list *curr;
+
+    curr = lst;
+    while (curr->next != NULL)
+        curr = curr->next;
+    curr->next = new;
+}
