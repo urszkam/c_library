62af7d1102d61da26314ae5f10d4314d90818bc0
Ula <ursz.kam@gmail.com>
2024-01-22 10:20:26 +0100

    add func memchr

---
+++ ft_memchr.c
@@ -0,0 +1,19 @@
+/*
+** function scans the initial n bytes of the memory area
+** pointed to by s for the first instance of c. Both c and the bytes
+** are interpreted as unsigned char.
+*/
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	c = (unsigned char)c;
+	while (n--)
+	{
+		if (*(unsigned char *)s == c)
+			return (s);
+		s++;
+	}
+	return (0);
+}
