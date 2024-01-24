c15b1d1f7c9dad3e579869fc7ef2b2a1ef1b6749
Ula <ursz.kam@gmail.com>
2024-01-24 22:50:33 +0100

    style: add empty line at the end

--- ft_strchr.c
+++ ft_strchr.c
@@ -1,13 +1,13 @@
 /*
-** function returns a pointer to the first occurrence 
+** function returns a pointer to the first occurrence
 ** of the character c in the string s
 */
 
 char	*ft_strchr(const char *s, int c)
 {
-    while (*s && *s != c)
-        s++;
-    if (c == '\0' || *s == c)
-        return ((char *)s);
-    return (0);
+	while (*s && *s != c)
+		s++;
+	if (c == '\0' || *s == c)
+		return ((char *)s);
+	return (0);
 }
