ff28bca14b5a2bb82c7a2f4fd3714c382057ce27
Ula <ursz.kam@gmail.com>
2024-01-25 13:49:00 +0100

    fix: chceck for n==0

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -11,6 +11,8 @@ int ft_strncmp(const char *s1, const char *s2, size_t n)
 {
     size_t i;
 
+    if (!n)
+        return (0);
     i = 0;
     while (s1[i] == s2[i] && s1[i] && i < n - 1)
         i++;
