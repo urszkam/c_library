7f8f0a5efc944be11af0b54d4de3cc42b1a09caa
Ula <ursz.kam@gmail.com>
2024-01-26 20:33:23 +0100

    add missing prototypes

--- libft.h
+++ libft.h
@@ -27,6 +27,8 @@ size_t ft_strlcpy(char *dst, const char *src, size_t size);
 
 char *ft_strchr(const char *s, int c);
 char *ft_strrchr(const char *s, int c);
+char *ft_strjoin(char const *s1, char const *s2);
+char *ft_strtrim(char const *s1, char const *set);
 
 void ft_bzero(void *s, size_t n);
 void *ft_memset(void *s, int c, size_t n);
