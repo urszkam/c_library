5140e6d73a573fcabb9c4cf77c023ce9f84fb864
Ula <ursz.kam@gmail.com>
2024-01-28 00:56:56 +0100

    update prototypes list

--- libft.h
+++ libft.h
@@ -32,16 +32,18 @@ char *ft_strtrim(char const *s1, char const *set);
 char *ft_strnstr(const char *s1, const char *s2, size_t len);
 char *ft_substr(char const *s, unsigned int start, size_t len);
 char *ft_strmapi(char const *s, char (*f)(unsigned int, char));
-void ft_striteri(char *s, void (*f)(unsigned int, char *));
 
 void ft_bzero(void *s, size_t n);
 void *ft_memset(void *s, int c, size_t n);
 void *ft_memcpy(void *dest, const void *src, size_t n);
 void *ft_memmove(void *dest, const void *src, size_t n);
+void ft_striteri(char *s, void (*f)(unsigned int, char *));
 void ft_putstr_fd(char *s, int fd);
 void ft_putnbr_fd(int n, int fd);
 void ft_putendl_fd(char *s, int fd);
 void ft_putchar_fd(char c, int fd);
+void ft_lstclear(t_list **lst, void (*del)(void *));
+void ft_lstdelone(t_list *lst, void (*del)(void *));
 void ft_lstadd_back(t_list **lst, t_list *new);
 void ft_lstadd_front(t_list **lst, t_list *new);
 
