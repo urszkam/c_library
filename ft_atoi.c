0bd4ad0e5564a99b5d5a7a832f44d282a3a90caf
Ula <ursz.kam@gmail.com>
2024-01-30 23:17:47 +0100

    style: change spaces to tabs

--- ft_atoi.c
+++ ft_atoi.c
@@ -5,25 +5,25 @@
 #include "libft.h"
 #include <stdio.h>
 
-int ft_atoi(const char *nptr)
+int	ft_atoi(const char *nptr)
 {
-    long    res;
-    int     sign;
+	long	res;
+	int		sign;
 
-    while ((*nptr >= 9 && *nptr <= 13) || *nptr == 32)
-        nptr++;
-    sign = 1;
-    if (*nptr == '+' || *nptr == '-')
-    {
-        if (*nptr == '-')
-            sign *= -1;
-        nptr++;
-    }
-    res = 0;
-    while (*nptr && *nptr >= '0' && *nptr <= '9')
-    {
-        res = res * 10 + (*nptr - '0');
-        nptr++;
-    }
-    return (res * sign);
+	while ((*nptr >= 9 && *nptr <= 13) || *nptr == 32)
+		nptr++;
+	sign = 1;
+	if (*nptr == '+' || *nptr == '-')
+	{
+		if (*nptr == '-')
+			sign *= -1;
+		nptr++;
+	}
+	res = 0;
+	while (*nptr && *nptr >= '0' && *nptr <= '9')
+	{
+		res = res * 10 + (*nptr - '0');
+		nptr++;
+	}
+	return (res * sign);
 }
