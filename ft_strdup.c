8a2821c9a26fab07674bea521370b05cb4cb980e
Ula <ursz.kam@gmail.com>
2024-01-30 23:12:51 +0100

    fix: declare len

--- ft_strdup.c
+++ ft_strdup.c
@@ -3,14 +3,19 @@
 char *ft_strdup(const char *s)
 {
     size_t  i;
+    size_t  len;
     char *dest;
 
     dest = (char *) malloc((ft_strlen(s) + 1) * sizeof(char));
     if (!dest)
         return (0);
     i = 0;
+    len = ft_strlen(s);
     while (i < len)
-        dest[i] = s[i++];
+    {
+        dest[i] = s[i];
+        i++;
+    }
     dest[i] = '\0';
     return (dest);
 }
