4eafb00d56cfdae6fdd9d3b264b69b61b12036f4
Ula <ursz.kam@gmail.com>
2024-01-31 10:53:38 +0100

    refactor: change incrementation

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -14,12 +14,12 @@ char *ft_strjoin(char const *s1, char const *s2)
     dest = (char *) malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
     if (!dest)
         return (0);
-    i = 0;
-    while(s1[i])
-        dest[i] = s1[i++];
-    j = 0;
-    while(s2[j])
-        dest[i + j] = s2[j++];
+    i = -1;
+    while(s1[++i])
+        dest[i] = s1[i];
+    j = -1;
+    while(s2[++j])
+        dest[i + j] = s2[j];
     dest[i + j] = '\0';
     return (dest);
 }
