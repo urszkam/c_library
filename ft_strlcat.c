14146d4be2dfc3ecfe475f8f5b760bba4ec07b84
Ula <ursz.kam@gmail.com>
2024-02-03 22:59:28 +0100

    style: spaces to tabs

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -9,20 +9,20 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-    size_t	src_len;
-    size_t	dst_len;
-    size_t	i;
+	size_t	src_len;
+	size_t	dst_len;
+	size_t	i;
 
-    src_len = ft_strlen(src);
-    dst_len = ft_strlen(dst);
-    if (size <= dst_len)
-        return (size + src_len);
-    i = 0;
-    while (i < size - dst_len - 1 && src[i])
-    {
-        dst[dst_len + i] = src[i];
-        i++;
-    }
-    dst[dst_len + i] = '\0';
-    return (src_len + dst_len);
+	src_len = ft_strlen(src);
+	dst_len = ft_strlen(dst);
+	if (size <= dst_len)
+		return (size + src_len);
+	i = 0;
+	while (i < size - dst_len - 1 && src[i])
+	{
+		dst[dst_len + i] = src[i];
+		i++;
+	}
+	dst[dst_len + i] = '\0';
+	return (src_len + dst_len);
 }
