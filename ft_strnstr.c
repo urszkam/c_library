0092ecbf040ac41b4c777289567ccfae3f915764
Ula <ursz.kam@gmail.com>
2024-02-06 23:49:20 +0100

    style: spaces to tabs

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,23 +1,23 @@
 #include "libft.h"
 
-char    *ft_strnstr(const char *s1, const char *s2, size_t len)
+char	*ft_strnstr(const char *s1, const char *s2, size_t len)
 {
-    size_t  i;
-    size_t  j;
+	size_t	i;
+	size_t	j;
 
-    if (!*s2)
-        return ((char *) s1);
-    i = 0;
-    while (s1[i] && i < len)
-    {
-        j = 0;
-        while (i + j < len && s1[i + j] == s2[j] && s2[j])
-        {
-            if (s2[j + 1] == '\0')
-                return ((char *) s1 + i);
-            j++;
-        }
-        i++;
-    }
-    return (0);
+	if (!*s2)
+		return ((char *) s1);
+	i = 0;
+	while (s1[i] && i < len)
+	{
+		j = 0;
+		while (i + j < len && s1[i + j] == s2[j] && s2[j])
+		{
+			if (s2[j + 1] == '\0')
+				return ((char *) s1 + i);
+			j++;
+		}
+		i++;
+	}
+	return (0);
 }
