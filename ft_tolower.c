f70573ff6f1c55422223eafc7e1cf4fd21ba3f17
Ula <ursz.kam@gmail.com>
2024-02-07 23:46:50 +0100

    style: spaces to tabs

--- ft_tolower.c
+++ ft_tolower.c
@@ -4,7 +4,7 @@
 
 int	ft_tolower(int c)
 {
-    if (c >= 'A' && c <= 'Z')
-        c += 32;
-    return (c);
+	if (c >= 'A' && c <= 'Z')
+		c += 32;
+	return (c);
 }
--- ft_toupper.c
+++ ft_toupper.c
@@ -4,7 +4,7 @@
 
 int	ft_toupper(int c)
 {
-    if (c >= 'a' && c <= 'z')
-        c -= 32;
-    return (c);
+	if (c >= 'a' && c <= 'z')
+		c -= 32;
+	return (c);
 }
