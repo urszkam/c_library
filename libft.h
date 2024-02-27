3ddf08bf92f6b29bb0f4b828b0bf24393f224f2d
Urszula Kaminska <ukaminsk@c3r1s3.42warsaw.pl>
2024-02-27 22:03:28 +0100

    add missing prototypes

--- libft.h
+++ libft.h
@@ -10,16 +10,17 @@ typedef struct s_list
     struct s_list *next;
 } t_list;
 
-int ft_isalnum(int c);
-int ft_isalpha(int c);
-int ft_isascii(int c);
-int ft_isdigit(int c);
-int ft_isprint(int c);
-int ft_toupper(int c);
-int ft_tolower(int c);
-int ft_strncmp(const char *s1, const char *s2, size_t n);
-int ft_atoi(const char *nptr);
-int ft_lstsize(t_list *lst);
+int	ft_isalnum(int c);
+int	ft_isalpha(int c);
+int	ft_isascii(int c);
+int	ft_isdigit(int c);
+int	ft_isprint(int c);
+int	ft_toupper(int c);
+int	ft_tolower(int c);
+int	ft_strncmp(const char *s1, const char *s2, size_t n);
+int	ft_memcmp(const void *s1, const void *s2, size_t n);
+int	ft_atoi(const char *nptr);
+int	ft_lstsize(t_list *lst);
 
 size_t ft_strlen(const char *s);
 size_t ft_strlcat(char *dst, const char *src, size_t size);
@@ -37,6 +38,7 @@ void ft_bzero(void *s, size_t n);
 void *ft_memset(void *s, int c, size_t n);
 void *ft_memcpy(void *dest, const void *src, size_t n);
 void *ft_memmove(void *dest, const void *src, size_t n);
+void *ft_memchr(const void *s, int c, size_t n);
 void ft_striteri(char *s, void (*f)(unsigned int, char *));
 void ft_putstr_fd(char *s, int fd);
 void ft_putnbr_fd(int n, int fd);
