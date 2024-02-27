5856b2b4a47087b0d8f16dfd282162175538137a
Urszula Kaminska <ukaminsk@c3r1s3.42warsaw.pl>
2024-02-27 22:07:33 +0100

    refactor: change returned value

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -10,6 +10,11 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+** The memcpy() function copies n bytes from memory area src to
+** memory area dest. The memory areas must not overlap.
+*/
+
 #include "libft.h"
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
@@ -18,10 +23,10 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	unsigned char	*src_ptr;
 
 	if (!dest && !src)
-		return (0);
+		return (dest);
 	dest_ptr = (unsigned char *)dest;
 	src_ptr = (unsigned char *)src;
-	while (n-- > 0)
-		*(dest_ptr++) = *src_ptr++;
+	while (n--)
+		*(dest_ptr++) = *(src_ptr++);
 	return (dest);
 }
