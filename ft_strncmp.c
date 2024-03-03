e3707399474259eb5993f9eb11b321052837c1f2
Ula <ursz.kam@gmail.com>
2024-03-03 12:11:26 +0100

    cast type to unsigned char

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -28,5 +28,5 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	while (s1[i] == s2[i] && s1[i] && i < n - 1)
 		i++;
-	return (s1[i] - s2[i]);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
